9c1d900c968e2b9e2d2c1df25330cee19fa7f07d
mikekang <ksflex@qdis.co.kr>
2025-12-12 18:50:04 +0900

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/vnode-editor/",
+  build: {
+    outDir: 'docs',            // GitHub Pages가 읽을 폴더
+  },
   plugins: [react()],
 })
